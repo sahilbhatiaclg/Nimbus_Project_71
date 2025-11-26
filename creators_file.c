c2b9f86cc0beebfdbd92dedf26b20dabda3295cb
sahilbhatia <sahilbhatiapcacc@gmail.com>
2025-11-26 23:28:45 +0530

    first commit by sb

---
+++ creators_file.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+ //STRUCTURE DEFINATION
+
+
+typedef struct {
+    char name[50];
+    double watt;
+    double hrs;
+    int on;
+
+}thing;
